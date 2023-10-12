59f69b5db29cd32e11cfc94a8d2d849a223e55d5
LoveWei0 <xdw666888@qq.com>
2023-10-12 10:45:35 +0800

    feat: show windtail

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 // components
 // import ReadPdf from './pages/readPdf/ReadPdf'
+// css
+import './App.css'
 
 export default function App() {
   return (
     <>
       {/* readPdf组件 */}
       {/* <ReadPdf /> */}
-      App
+      <p className="btn-primary">App</p>
+      <div className="bg-red-500 grid gap-4 grid-cols-3  grid-rows-3">
+        <p>01</p>
+        <p>02</p>
+        <p>03</p>
+        <p>04</p>
+        <p>05</p>
+        <p>06</p>
+        <p>07</p>
+        <p>08</p>
+        <p>09</p>
+      </div>
     </>
   )
 }
