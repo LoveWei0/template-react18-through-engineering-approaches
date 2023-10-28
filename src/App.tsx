612d49bb8790400f918d1c08b90700b5e1065dc6
lovewei0 <xdw666888@qq.com>
2023-10-28 15:41:16 +0800

    feat: capsulation errorMange

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from 'react'
-// components
-// import ReadPdf from './pages/readPdf/ReadPdf'
-// css
-import './App.css'
+// import PdfCom from './components/PdfCoM'
+import ErrorComponent from './utils/errorManage'
 
+const dd = () => {
+  return <div>d</div>
+}
 export default function App() {
-  return (
-    <>
-      {/* readPdf组件 */}
-      {/* <ReadPdf /> */}
-      <p className="btn-primary">App</p>
-      <div className="bg-red-500 grid gap-4 grid-cols-3  grid-rows-3">
-        <p>01</p>
-        <p>02</p>
-        <p>03</p>
-        <p>04</p>
-        <p>05</p>
-        <p>06</p>
-        <p>07</p>
-        <p>08</p>
-        <p>09</p>
-      </div>
-    </>
-  )
+  return <>{ErrorComponent(dd)}</>
 }
